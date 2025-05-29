69691fc22f043a1f5ed137ca703c0b9c5b10fe9e
Trailblazer Isaiah <isaiahmwalker@icloud.com>
2025-05-29 15:09:32 -0700

    Minesweeper game for fun and teaching

---
+++ block.gd
@@ -0,0 +1,119 @@
+extends Sprite2D
+
+# blocks detect when they are clicked on by the user
+# (or space bar is pressed on it).
+
+# then it recursively blows up surrounding blocks,
+# revealing the empty spaces until you reach edges with numbers
+
+
+# TODO: bomb explosion when you click a bomb (particles/smoke effect)
+
+var covered: bool = true
+var bomb = false
+var flag = false
+
+static var alive = true
+
+static var first_click = true
+
+var clicking: bool = false
+
+@export var board_pos: Vector2i = Vector2i(0, 0)
+
+signal opened(to_open: Vector2i) # emitted when this block is opened
+
+func _input(event):
+	if !alive: return
+	
+	# space bar toggles a flag on the currently hovered block
+	if Input.is_action_just_pressed("toggle_flag") && covered && get_rect().has_point(get_global_mouse_position() - position):
+		flag = !flag
+		if flag:
+			var flag_sprite = Sprite2D.new()
+			flag_sprite.name = "Flag"
+			flag_sprite.texture = load("res://Flag.png")
+			flag_sprite.scale = Vector2(0.7, 0.7)
+			add_child(flag_sprite)
+			get_parent().bombs_found += 1
+			get_parent().update_bombs_msg()
+		else:
+			var flag_sprite = $Flag
+			remove_child(flag_sprite)
+			flag_sprite.queue_free()
+			get_parent().bombs_found -= 1
+			get_parent().update_bombs_msg()
+	
+	if !flag && Input.is_action_just_pressed("click") && covered && get_rect().has_point(event.position - position):
+		clicking = true
+		$AnimationPlayer.play("grow")
+	
+	if Input.is_action_just_released("click") && covered && clicking:
+		clicking = false
+		if get_rect().has_point(event.position - position):
+			if first_click:
+				first_click = false
+				get_parent().emit_signal("first_click", board_pos)
+			covered = false
+			reveal()
+			#print("You opened the square")
+			$CPUParticles2D.emitting = true
+			await get_tree().create_timer(0.08).timeout
+			frame = 1
+			scale = Vector2(1, 1)
+			
+			if bomb:
+				print("Game over")
+				alive = false
+				get_node("Explosion").emitting = true
+				get_parent().game_end()
+			
+			if !bomb && !bomb_count():
+				opened.emit(board_pos)
+		else:
+			$AnimationPlayer.play_backwards("grow")
+
+
+## returns a list of the adjacent blocks to this one
+func get_adjs() -> Array[Vector2i]:
+	var adjs: Array[Vector2i] = []
+	for i in range(max(0, board_pos.x-1), min(get_parent().board_dim.x, board_pos.x+2)):
+		for j in range(max(0, board_pos.y-1), min(get_parent().board_dim.y, board_pos.y+2)):
+			var pair = Vector2i(i, j)
+			if pair != board_pos: adjs.append(pair)
+	return adjs
+
+
+## count the number of bombs around this block
+func bomb_count() -> int:
+	return len(get_adjs()\
+	.map(func(pos): return get_parent().board[pos.y][pos.x])\
+	.filter(func(block): return block.bomb))
+
+
+## connect signals properly to adjacent blocks for recursive opening
+func connect_to_adjs():
+	for adj in get_adjs():
+		opened.connect(get_parent().get_block(adj).on_opened)
+
+
+## callback function for what happens when a nearby block opens up
+func on_opened(pos):
+	if !flag && covered && pos in get_adjs():
+		$AnimationPlayer.play("grow")
+		covered = false
+		reveal()
+		$CPUParticles2D.emitting = true
+		await get_tree().create_timer(0.08).timeout
+		frame = 1
+		scale = Vector2.ONE
+		if !bomb && !bomb_count():
+			opened.emit(board_pos)
+
+
+func reveal():
+	var bomb_child = get_node("Bomb")
+	if bomb_child:
+		bomb_child.visible = true
+	else:
+		$Label.visible = true
---
+++ minesweeper.gd
@@ -0,0 +1,94 @@
+extends Node2D
+
+static var board_dim: Vector2 = Vector2(25, 15)
+@onready var board = []
+var start = Vector2i(115, 107)
+
+static var bombs = 0
+static var bombs_found = 0
+
+var block_data = load("res://block.tscn")
+
+signal first_click(pos)
+
+func _ready():
+	# make blocks
+	for i in range(board_dim.y):
+		var row = []
+		for j in range(board_dim.x):
+			var new_block = block_data.instantiate()
+			new_block.board_pos = Vector2i(j, i)
+			new_block.get_node("Label").text = ""
+			row.append(new_block)
+			add_child(new_block)
+			new_block.position = start + (new_block.board_pos * 32)
+		board.append(row) # 2d list can now refer to all block by pos
+	
+	# wait for the first click before setting up
+	var open_block = await first_click
+	print("Clicked")
+	
+	for row in board: for block in row:
+		if abs(block.board_pos.x - open_block.x) + abs(block.board_pos.y - open_block.y) >= 3:
+			if randi_range(0, 3) == 3:
+				block.bomb = true
+				var expl = load("res://explosion.tscn").instantiate()
+				expl.name = "Explosion"
+				block.add_child(expl)
+				bombs += 1
+	
+	# connect all signals, set images for blocks
+	for row in board: for block in row:
+		# connect signals
+		block.connect_to_adjs()
+		
+		var bomb_count = block.bomb_count()
+		# check if this is a bomb. if it is, put a bomb sprite on it.
+		if block.bomb:
+			block.get_node("Label").text = ""
+			var bomb_sprite = Sprite2D.new()
+			bomb_sprite.texture = load("res://Bomb.png")
+			block.add_child(bomb_sprite)
+			bomb_sprite.scale = Vector2(0.7, 0.7)
+			bomb_sprite.visible = false
+			bomb_sprite.name = "Bomb"
+		# if not a bomb, then put the number of bombs adjacent to itself
+		elif bomb_count:
+			block.get_node("Label").text = "%d" % bomb_count
+			block.get_node("Label").visible = false
+		# if no surrounding bombs, leave it empty
+		else:
+			block.get_node("Label").text = ""
+	
+	$Label.text = "Remaining: %d" % (bombs - bombs_found)
+
+
+## given a position, return the matching block in the board
+func get_block(pos: Vector2i):
+	return board[pos.y][pos.x]
+
+
+func update_bombs_msg(): $Label.text = "Remaining: %d" % (bombs - bombs_found)
+
+
+func game_end():
+	var flattened_board: Array = []
+	for row in board:
+		flattened_board.append_array(row)
+	flattened_board.shuffle()
+	
+	for block in flattened_board:
+		if block.flag: block.remove_child(get_node("Flag")); block.flag = false
+		
+		if block.covered:
+			block.get_node("AnimationPlayer").play("grow")
+			block.covered = false
+			block.reveal()
+			await get_tree().create_timer(0.001).timeout
+			block.get_node("CPUParticles2D").emitting = true
+			block.frame = 1
+			block.scale = Vector2.ONE
+		
+		if block.bomb:
+			block.get_node("Explosion").emitting = true
+			$Camera2D/AnimationPlayer.play("screen_shake")
